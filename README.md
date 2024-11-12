9071dba3f542165994e70c0981ae4fe4718f26a9
Jose <73000873+Typheta@users.noreply.github.com>
2024-11-12 18:33:49 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# SE-2024Spring-Team-10
+Qwvontae Graves
+Sadiq Toussaint
+Jose Espino
---
+++ ScrabbleBase/src/DataAccess/PlayerDataAccess.java
@@ -0,0 +1,66 @@
+package DataAccess;
+
+import java.util.ArrayList;
+
+import DataObject.PlayerDataObject;
+import restService.Message;
+
+public class PlayerDataAccess {
+
+	private static ArrayList<PlayerDataObject> players;
+
+	public PlayerDataAccess() {
+		initialize();
+	}
+
+	private void initialize() {
+		players = new ArrayList<PlayerDataObject>();
+	
+		//Default Data
+        //R. Florin
+		players.add(new PlayerDataObject(1, "rflorin", "pass1"));
+		//K. Findley
+		players.add(new PlayerDataObject(2, "kfindley", "pass2"));
+		//Player not used on any game.
+		players.add(new PlayerDataObject(3, "unused", "pass2"));
+
+        
+	}
+
+	private static int getNextId() {
+		return players.size()+1;
+	}
+
+	public static ArrayList<PlayerDataObject> getAllPlayers() {
+		return players;
+	}
+
+	public static PlayerDataObject getPlayerById(int id) {
+		
+		for (int i=0; i < players.size(); i++) {
+			if (players.get(i).id == id) {
+				return players.get(i);
+			}
+		}
+		
+		return null;
+	}
+
+	public static PlayerDataObject getPlayerByUsername(String username) {
+		
+		for (int i=0; i < players.size(); i++) {
+			if (players.get(i).userName.equals(username)) {
+				return players.get(i);
+			}
+		}
+		
+		return null;
+	}
+
+	public static PlayerDataObject createPlayer(PlayerDataObject data) {
+
+		data.id = getNextId();
+		players.add(data);
+		return data;
+	}
+}
